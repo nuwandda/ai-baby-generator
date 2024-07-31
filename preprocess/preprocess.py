d5d2ad206ed84a60055b070f8fc3343172a2cf23
nuwandda <donmezbugrahan@gmail.com>
2024-07-31 22:56:42 +0300

    Add no face check

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -60,47 +60,51 @@ def preprocess(im: np.ndarray, focal: float) -> str:
         str: The gaze direction of the person
 
     """
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(PREDICTOR_PATH)
+    try:
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
-    faces = detector(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), 0)
-    face3Dmodel = world.ref3DModel()
+        faces = detector(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), 0)
+        face3Dmodel = world.ref3DModel()
 
-    for face in faces:
-        shape = predictor(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), face)
-        refImgPts = world.ref2dImagePoints(shape)
+        for face in faces:
+            shape = predictor(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), face)
+            refImgPts = world.ref2dImagePoints(shape)
 
-        height, width, channel = im.shape
-        focalLength = focal * width
-        cameraMatrix = world.cameraMatrix(focalLength, (height / 2, width / 2))
+            height, width, channel = im.shape
+            focalLength = focal * width
+            cameraMatrix = world.cameraMatrix(focalLength, (height / 2, width / 2))
 
-        mdists = np.zeros((4, 1), dtype=np.float64)
+            mdists = np.zeros((4, 1), dtype=np.float64)
 
-        # Calculate rotation and translation vector using solvePnP
-        success, rotationVector, translationVector = cv2.solvePnP(
-            face3Dmodel, refImgPts, cameraMatrix, mdists)
+            # Calculate rotation and translation vector using solvePnP
+            success, rotationVector, translationVector = cv2.solvePnP(
+                face3Dmodel, refImgPts, cameraMatrix, mdists)
 
-        noseEndPoints3D = np.array([[0, 0, 1000.0]], dtype=np.float64)
-        noseEndPoint2D, jacobian = cv2.projectPoints(
-            noseEndPoints3D, rotationVector, translationVector, cameraMatrix, mdists)
+            noseEndPoints3D = np.array([[0, 0, 1000.0]], dtype=np.float64)
+            noseEndPoint2D, jacobian = cv2.projectPoints(
+                noseEndPoints3D, rotationVector, translationVector, cameraMatrix, mdists)
 
-        # Draw nose line 
-        p1 = (int(refImgPts[0, 0]), int(refImgPts[0, 1]))
-        p2 = (int(noseEndPoint2D[0, 0, 0]), int(noseEndPoint2D[0, 0, 1]))
+            # Draw nose line 
+            p1 = (int(refImgPts[0, 0]), int(refImgPts[0, 1]))
+            p2 = (int(noseEndPoint2D[0, 0, 0]), int(noseEndPoint2D[0, 0, 1]))
 
-        # Calculating angle
-        rmat, jac = cv2.Rodrigues(rotationVector)
-        angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
-        x = np.arctan2(Qx[2][1], Qx[2][2])
-        y = np.arctan2(-Qy[2][0], np.sqrt((Qy[2][1] * Qy[2][1] ) + (Qy[2][2] * Qy[2][2])))
-        z = np.arctan2(Qz[0][0], Qz[1][0])
+            # Calculating angle
+            rmat, jac = cv2.Rodrigues(rotationVector)
+            angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+            x = np.arctan2(Qx[2][1], Qx[2][2])
+            y = np.arctan2(-Qy[2][0], np.sqrt((Qy[2][1] * Qy[2][1] ) + (Qy[2][2] * Qy[2][2])))
+            z = np.arctan2(Qz[0][0], Qz[1][0])
 
-        gaze = "Looking: "
-        if angles[1] < -15:
-            gaze += "Left"
-        elif angles[1] > 15:
-            gaze += "Right"
-        else:
-            gaze += "Forward"
+            gaze = "Looking: "
+            if angles[1] < -15:
+                gaze += "Left"
+            elif angles[1] > 15:
+                gaze += "Right"
+            else:
+                gaze += "Forward"
 
-    return gaze, angles[1]
+        return gaze, angles[1]
+    except Exception as e:
+        print(f"[ERROR] {e}")
+        return False, -1
--- services.py
+++ services.py
@@ -17,28 +17,52 @@ from file_operations import download_folder_from_s3
 
 
 TEMP_PATH = 'temp'
-# S3 bucket name
-bucket_name = 'aibabygenerator-models'
-# Folder in S3 to download
-folder_name = 'aiBaby/reference_photos'
-# Local directory to save the files
-local_dir = 'reference_photos'
-# Download the folder from S3
-if not os.path.exists('reference_photos'):
-    download_folder_from_s3(bucket_name, folder_name, local_dir)
 
 # Helper functions
 def create_temp():
+    """
+    Creates a temporary directory if it does not exist.
+
+    This function checks if the directory specified by the `TEMP_PATH` constant exists.
+    If the directory does not exist, it creates the directory using the `os.makedirs()` function.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     if not os.path.exists(TEMP_PATH):
         os.makedirs(TEMP_PATH)
 
 
-def remove_temp_image(id):
+def remove_temp_image(id: str) -> None:
+    """
+    Removes temporary images from the specified temporary directory.
+
+    Parameters:
+    id (str): The unique identifier for the baby images. This identifier is used to construct the file names of the temporary images.
+
+    Returns:
+    None: This function does not return any value. It only removes the temporary images.
+    """
     os.remove(TEMP_PATH + '/' + id + '_child.png')
-    os.remove(TEMP_PATH + '/' + id + '_hair.png')
 
 
 def image_to_base64(pil_image):
+    """
+    Converts a PIL image to a base64-encoded string.
+
+    This function takes a PIL image as input, converts it to bytes, encodes the bytes to base64,
+    and finally decodes the base64 bytes to a UTF-8 string. The resulting string can be used
+    in various contexts, such as sending images over a network or storing them in a database.
+
+    Parameters:
+    pil_image (PIL.Image): The input PIL image to be converted to base64.
+
+    Returns:
+    str: A base64-encoded string representation of the input PIL image.
+    """
     # Create a BytesIO object to temporarily hold the image data
     img_byte_array = io.BytesIO()
 
@@ -58,8 +82,21 @@ def image_to_base64(pil_image):
 
 
 async def generate_image(babyCreate: _schemas.BabyCreate) -> Image:
+    """
+    This function generates a baby image based on the input parameters. It performs preprocessing,
+    image generation, and postprocessing steps to produce a realistic baby image.
+
+    Parameters:
+    babyCreate (_schemas.BabyCreate): An object containing the necessary information for generating the baby image.
+        This object includes attributes such as encoded_dad_imgs, encoded_mom_imgs, img_height, focal_length, token,
+        gender, power_of_dad, ethnicity, total_number_of_photos, and skin_tone.
+
+    Returns:
+    Image: The generated baby image.
+    """
     temp_id = str(uuid.uuid4())
     create_temp()
+    message = 'Successfully generated baby image.'
 
     """
         ---PREPROCESS---
@@ -80,6 +117,10 @@ async def generate_image(babyCreate: _schemas.BabyCreate) -> Image:
             resized_image = init_image.resize((babyCreate.img_height, target_height), Image.ANTIALIAS)
 
         _, score = preprocess.preprocess(np.array(resized_image), babyCreate.focal_length)
+        # We check if the face is not proper in the line below and continue with the next one
+        if score == -1:
+            continue
+
         if score < current_father_score:
             current_father_score = score
             best_father_image = resized_image
@@ -99,6 +140,10 @@ async def generate_image(babyCreate: _schemas.BabyCreate) -> Image:
             resized_image = init_image.resize((babyCreate.img_height, target_height), Image.ANTIALIAS)
 
         _, score = preprocess.preprocess(np.array(resized_image), babyCreate.focal_length)
+        # We check if the face is not proper in the line below and continue with the next one
+        if score == -1:
+            continue
+
         if score < current_mother_score:
             current_mother_score = score
             best_mother_image = resized_image
@@ -107,6 +152,10 @@ async def generate_image(babyCreate: _schemas.BabyCreate) -> Image:
         ---PROCESS---
         Generate the baby using the best images
     """
+    # Returns None and message if one of the best images are missing
+    if best_father_image is None or best_mother_image is None:
+        return None, "One of the parent images is missing!"
+     
     father_encoded_img = image_to_base64(best_father_image)
     mother_encoded_img = image_to_base64(best_mother_image)
     
@@ -115,31 +164,12 @@ async def generate_image(babyCreate: _schemas.BabyCreate) -> Image:
     
     child.save(TEMP_PATH + '/' + temp_id + '_child.png')
 
-    # Opens a image in RGB mode
-    # child = Image.open('baby_test2.png')
-    
-    # Size of the image in pixels (size of original image)
-    # (This is not mandatory)
-    width, height = child.size
-    
-    # Setting the points for cropped image
-    left = 270
-    top = 0
-    right = 720
-    bottom = 150
-    
-    # Cropped image of above dimension
-    # (It will not change original image)
-    im1 = child.crop((left, top, right, bottom))
-    im1.save(TEMP_PATH + '/' + temp_id + '_hair.png')
-    hair_color = get_hair_color(TEMP_PATH + '/' + temp_id + '_hair.png', babyCreate.skin_tone)
-
     """
         ---POSTPROCESS---
         Generate realistic baby picture using face swap and Stable Diffusion
     """
-    return_images = generate(TEMP_PATH + '/' + temp_id + '_child.png', temp_id, babyCreate.gender, babyCreate.total_number_of_photos, hair_color, babyCreate.skin_tone)
+    return_images = generate(TEMP_PATH + '/' + temp_id + '_child.png', temp_id, babyCreate.gender, babyCreate.total_number_of_photos, babyCreate.skin_tone)
 
     remove_temp_image(temp_id)
-    return return_images
+    return return_images, message
         
